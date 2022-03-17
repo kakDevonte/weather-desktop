6c5cd6f1ba019d5cf93d1d61e51d25d01e73c513
kakDevonte <kakdevonte@gmail.com>
2022-03-17 22:52:22 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState} from 'react'
+import {Header} from './components/Header/Header'
+import {Weather} from './components/Weather/Weather'
+import {Footer} from './components/Footer/Footer'
+import {useWeatherActions} from './contexts/weather-context'
+import {getAddress} from './utils/current-geolocation'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const { fetchWeatherDataByCoordinate } = useWeatherActions()
+
+    useEffect(() => {
+        const fetchCoordinate = async () => {
+            const { lat, lon } = await getAddress()
+            fetchWeatherDataByCoordinate(lat, lon)
+        }
+        fetchCoordinate()
+    }, [])
+
+    return (
+        <div className="App">
+            <div className="gridItem">
+                <Header/>
+            </div>
+            <div className="gridItem">
+                <Weather/>
+            </div>
+            <div className="gridItem">
+                <Footer />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
